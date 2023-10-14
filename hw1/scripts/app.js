608aa5444d69ca167b3175ebba90bd2526ec1f27
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2023-10-14 12:08:32 +0300

    added parts 6/10

--- hw1/scripts/app.js
+++ hw1/scripts/app.js
@@ -1,85 +1,290 @@
-import { getCoordinatesOfIsoscelesTriangles, NUMBER_OF_SQUARES_IN_A_ROW, INITIAL_COLOR } from './utils.js'; 
+/*
+1 done
+2 done
+3 done
+4 done
+5 eraser
+6 done
+7 rectangular selection
+8 rectangular selection copy
+9 3 layers
+10 buggy
+*/
 
-const canvas = document.getElementById('gl-canvas');
-const gl = canvas.getContext('webgl');
+var canvas;
+var gl;
 
-if (!gl) {
-    alert('Your browser does not support WebGL');
+var scale = 1;
+var maxNumTriangles = 3600;
+var maxNumVertices = 3 * maxNumTriangles;
+var index = 0;
+UNIT_SQUARE_DIM = 20
+var MAX_NUMBER_OF_UNDO = 20;
+var MAX_NUMBER_OF_REDO = 20;
+var undoIndexArray = [];
+var undoCounter = 0;
+var redoIndexArray = [];
+var saveVertexData = [];
+var saveColorData = [];
+
+[0,0,0,0,0,1,0,0,0,1,0,0]
+
+var redraw = false;
+
+var colors = [
+  vec4(0.0, 0.0, 0.0, 1.0),  // black
+  vec4(1.0, 0.0, 0.0, 1.0),  // red
+  vec4(0.0, 1.0, 0.0, 1.0),  // green
+  vec4(0.0, 0.0, 1.0, 1.0),  // blue
+  vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+  vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+  vec4(0.0, 1.0, 1.0, 1.0)   // cyan
+];
+
+var selectedColor = colors[0];
+
+function transformPoints(x, y) {
+  var t = vec2(2 * x / canvas.width - 1,
+    2 * (canvas.height - y) / canvas.height - 1);
+  return t;
 }
 
-const vertexData = [];
+window.onload = function init() {
+  canvas = document.getElementById("gl-canvas");
 
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) { alert("WebGL isn't available"); }
 
-// make the vertex points such that the whole canvas is a 30x30 grid
-for( let i = 0; i <= NUMBER_OF_SQUARES_IN_A_ROW; i++ ){
-    for( let j = 0; j < NUMBER_OF_SQUARES_IN_A_ROW; j++ ){
-        vertexData.push(...getCoordinatesOfIsoscelesTriangles(
-            -1 + 2*i/NUMBER_OF_SQUARES_IN_A_ROW, -1 + 2*j/NUMBER_OF_SQUARES_IN_A_ROW,
-            -1 + 2*(i+1)/NUMBER_OF_SQUARES_IN_A_ROW, -1 + 2*(j+1)/NUMBER_OF_SQUARES_IN_A_ROW
-        )); 
+  canvas.addEventListener("mousedown", function (event) {
+    redraw = scale === 1 ? true : false; 
+  });
+
+  canvas.addEventListener("mouseup", function (event) {
+    redraw = false;
+    if (undoIndexArray.length == MAX_NUMBER_OF_UNDO) {
+      undoIndexArray.shift();
     }
-} 
+    undoIndexArray.push(undoCounter);
+    undoCounter = 0;
+  });
+  
+  canvas.addEventListener("mousemove", function (event) {
+
+    if (redraw) {
+      var squareX = Math.floor(event.clientX / UNIT_SQUARE_DIM);
+      var squareY = Math.floor(event.clientY / UNIT_SQUARE_DIM);
+      // Calculate the center of the square unit
+      var squareCenterX = squareX * UNIT_SQUARE_DIM + (UNIT_SQUARE_DIM / 2);
+      var squareCenterY = squareY * UNIT_SQUARE_DIM + (UNIT_SQUARE_DIM / 2);
+
+      // Calculate the relative distance from the mouse to the square center
+      var deltaX = event.clientX - squareCenterX;
+      var deltaY = event.clientY - squareCenterY;
+      // Determine which triangle within the square the mouse is on
+      let vertices = null;
+      if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        // Mouse is closer to the left or right triangle
+        if (deltaX < 0) {
+          vertices = [
+            transformPoints(squareX * UNIT_SQUARE_DIM, squareY * UNIT_SQUARE_DIM),
+            transformPoints(squareX * UNIT_SQUARE_DIM, (squareY + 1) * UNIT_SQUARE_DIM),
+            transformPoints(squareCenterX, squareCenterY)
+          ];
+        } else {
+          vertices = [
+            transformPoints((squareX + 1) * UNIT_SQUARE_DIM, squareY * UNIT_SQUARE_DIM),
+            transformPoints((squareX + 1) * UNIT_SQUARE_DIM, (squareY + 1) * UNIT_SQUARE_DIM),
+            transformPoints(squareCenterX, squareCenterY)
+          ];
+        }
+      } else {
+        // Mouse is closer to the top or bottom triangle
+        if (deltaY < 0) {
+          vertices = [
+            transformPoints(squareX * UNIT_SQUARE_DIM, squareY * UNIT_SQUARE_DIM),
+            transformPoints((squareX + 1) * UNIT_SQUARE_DIM, squareY * UNIT_SQUARE_DIM),
+            transformPoints(squareCenterX, squareCenterY)
+          ];
+        } else {
+          vertices = [
+            transformPoints(squareX * UNIT_SQUARE_DIM, (squareY + 1) * UNIT_SQUARE_DIM),
+            transformPoints((squareX + 1) * UNIT_SQUARE_DIM, (squareY + 1) * UNIT_SQUARE_DIM),
+            transformPoints(squareCenterX, squareCenterY)
+          ];
+        }
+      }
+      undoCounter = undoCounter + 3;
+      gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8 * index, flatten(vertices));
+      saveVertexData.push(...vertices.flat());
+      gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+      vertices_color = [vec4(selectedColor),
+      vec4(selectedColor),
+      vec4(selectedColor)];
+      saveColorData.push(...vertices_color.flat());
+      gl.bufferSubData(gl.ARRAY_BUFFER, 16 * index, flatten(vertices_color));
+      index = (index + 3);
+    }
+
+  });
+
+  document.getElementById("ColorPicker").onclick = function (event) {
+    switch (event.target.index) {
+      case 0:
+        selectedColor = colors[0];
+        break;
+      case 1:
+        selectedColor = colors[1];
+        break;
+      case 2:
+        selectedColor = colors[2];
+        break;
+      case 3:
+        selectedColor = colors[3];
+        break;
+      case 4:
+        selectedColor = colors[4];
+        break;
+      case 5:
+        selectedColor = colors[5];
+        break;
+      case 6:
+        selectedColor = colors[6];
+        break;
+    }
+  };
+
+  document.getElementById("ZoomControls").onclick = function (event) {
+    switch (event.target.index) {
+      case 0:
+        scale += 0.2;
+        break;
+      case 1:
+        scale -= 0.2;
+        break;
+    }
+    scaleLoc = gl.getUniformLocation(program, "scale");
+    gl.uniform1f(scaleLoc, scale);
+  };
+
+  document.getElementById("ClearButton").onclick = function (event) {
+    index = 0;
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    undoIndexArray = [];
+    undoCounter = 0;
+    redoIndexArray = [];
+  };
+
+  document.getElementById("save").onclick = function (event) {
+    // Save and load options for the current canvas. It is enough to keep the vertex, index, and color information; 
+    // you do not need to save the scale or the selected color.
+    var saveData = {
+        "index": index * 8,
+        "vertices": flatten(saveVertexData),
+        "colors": flatten(saveColorData)
+    };
+
+    //download save data as a file
+    var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(saveData));
+    var a = document.createElement('a');
+    a.href = 'data:' + data;
+    a.download = 'data.json';
+    a.innerHTML = 'download JSON';
+    a.click();
+
+    console.log(saveData);
+  };
+  
+  document.getElementById("load").onchange = function (event) {
+    // Load the saved data into the current canvas. 
+    // You should clear the current canvas before loading the saved data.
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        var saveData = JSON.parse(event.target.result);
+        index = saveData.index;
+        newVertices = Object.values(saveData.vertices);
+        newColors = Object.values(saveData.colors);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(newVertices));
+        gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(newColors));
+        gl.drawArrays(gl.TRIANGLES, 0, index);
+    };
+    reader.readAsText(file);
+  };
+
+   document.getElementById("undo").onclick = function (event) {
+       if (undoIndexArray.length > 0) {
+           var decrease_index = undoIndexArray.pop();
+           redoIndexArray.push(decrease_index);
+           index -= decrease_index;
+           gl.clear(gl.COLOR_BUFFER_BIT);
+           gl.drawArrays(gl.TRIANGLES, 0, index);
+       } else {
+           alert("Nothing to undo");
+       }
+   };
+
+   document.getElementById("redo").onclick = function (event) {
+       if (redoIndexArray.length > 0) {
+            var increase_index = redoIndexArray.pop();
+            undoIndexArray.push(increase_index);
+            index += increase_index;
+            gl.clear(gl.COLOR_BUFFER_BIT);
+            gl.drawArrays(gl.TRIANGLES, 0, index);
+         } else {
+            alert("Nothing to redo");
+        }
+
+    }
 
 
-const colorData = [];
-for( let i = 0; i < vertexData.length/3; i++ ){
-    colorData.push(0,1,1);
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1, 1, 1, 1.0);
+
+
+  //
+  //  Load shaders and initialize attribute buffers
+  //
+  var program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
+
+
+  var vBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, 8 * maxNumVertices, gl.STATIC_DRAW);
+
+  var vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  scaleLoc = gl.getUniformLocation(program, "scale");
+  gl.uniform1f(scaleLoc, scale);
+
+
+  var cBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, 16 * maxNumVertices, gl.STATIC_DRAW);
+
+  var vColor = gl.getAttribLocation(program, "vColor");
+  gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vColor);
+
+  render();
+
 }
 
-const positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
 
+function render() {
 
-const colorBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, index);
 
-
-const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-gl.shaderSource(vertexShader, `
-precision mediump float;
-attribute vec3 position;
-varying vec3 fColor;
-attribute vec3 vColor;
-
-void main() {
-    gl_Position = vec4(position,  1);
-    fColor = vColor;
+  setTimeout(
+    function () { requestAnimFrame(render); },
+    50
+  );
 
 }
-`);
-gl.compileShader(vertexShader);
-
-const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-gl.shaderSource(fragmentShader, `
-precision mediump float;
-varying vec3 fColor;
-
-void main() {
-    gl_FragColor = vec4(fColor, 1);
-}
-`);
-gl.compileShader(fragmentShader);
-console.log(gl.getShaderInfoLog(fragmentShader));
-
-const program = gl.createProgram();
-gl.attachShader(program, vertexShader);
-gl.attachShader(program, fragmentShader);
-
-gl.linkProgram(program);
-
-const positionLocation = gl.getAttribLocation(program, `position`);
-gl.enableVertexAttribArray(positionLocation);
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
-
-
-const colorLocation = gl.getAttribLocation(program, `vColor`);
-gl.enableVertexAttribArray(colorLocation);
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
-
-
-gl.useProgram(program);
-gl.drawArrays(gl.TRIANGLES, 0, vertexData.length/3);
--- hw1/scripts/utils.js
+++ hw1/scripts/utils.js
@@ -1,5 +1,5 @@
 export const NUMBER_OF_SQUARES_IN_A_ROW = 30;
-export const INITIAL_COLOR = [0.0, 0.0, 0.0];
+export const INITIAL_COLOR = [1.0, 1.0, 1.0];
 
 export const getCoordinatesOfIsoscelesTriangles = (x_start, y_start, x_end, y_end) => {
     return [
@@ -28,12 +28,57 @@ export const getCoordinatesOfIsoscelesTriangles = (x_start, y_start, x_end, y_en
 
 
 export const getRelativeIsoscelesTriangleIndex = (x_event, y_event) => {
-    x_event_relative = x_event * NUMBER_OF_SQUARES_IN_A_ROW;
-    y_event_relative = y_event * NUMBER_OF_SQUARES_IN_A_ROW;
+    const x_event_relative = x_event * NUMBER_OF_SQUARES_IN_A_ROW;
+    const y_event_relative = y_event * NUMBER_OF_SQUARES_IN_A_ROW;
 
-    triangle_x_start_index_relative = Math.floor(x_event_relative);
-    triangle_y_start_index_relative = Math.floor(y_event_relative);
+    const triangle_x_start_index_relative = Math.floor(x_event_relative);
+    const triangle_y_start_index_relative = Math.floor(y_event_relative);
 
+    // get the degree of angle between the mouse event and the center of the square
+    const x_center_relative = triangle_x_start_index_relative + 0.5;
+    const y_center_relative = triangle_y_start_index_relative + 0.5;
+
+    const x_difference_relative = x_event_relative - x_center_relative;
+    const y_difference_relative = y_event_relative - y_center_relative;
+
+    const angle = Math.atan2(y_difference_relative, x_difference_relative);
+
+    // top rectangle
+    let triangle_inside_square_index = 0;
+    if (y_difference_relative > 0) {
+        if (x_difference_relative > 0) {
+            if (angle >= Math.PI/4) {
+                triangle_inside_square_index = 2;
+            } else {
+                triangle_inside_square_index = 3;
+            }
+        } else {
+            if (angle >= Math.PI/4) {
+                triangle_inside_square_index = 2;
+            } else {
+                triangle_inside_square_index = 1;
+            }
+        }
+    } else {        // bottom rectangle
+        if (x_difference_relative > 0) {
+            if (angle >= Math.PI/4) {
+                triangle_inside_square_index = 0;
+            } else {
+                triangle_inside_square_index = 3;
+            }
+        } else {
+            if (angle >= Math.PI/4) {
+                triangle_inside_square_index = 0;
+            } else {
+                triangle_inside_square_index = 1;
+            }
+        }
+    }
+
+    let result = triangle_inside_square_index + 120 * (triangle_y_start_index_relative ) + 4 * triangle_x_start_index_relative;
+
+    if (result < 0) return 0;
+    if (result > 4 * NUMBER_OF_SQUARES_IN_A_ROW * NUMBER_OF_SQUARES_IN_A_ROW - 1) return 4 * NUMBER_OF_SQUARES_IN_A_ROW * NUMBER_OF_SQUARES_IN_A_ROW - 1;
     
-
+    return result
 }
